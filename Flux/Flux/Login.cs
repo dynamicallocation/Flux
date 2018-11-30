8d71c58454b2474ddc6a785619aa5216713af50f
DanielOsz <doszczapinski@luc.edu>
2018-11-29 20:31:02 -0600

    login and sign up

---
+++ Flux/Flux/Login.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Flux
+{
+    public partial class Login : Form
+    {
+        string connectionStrin = @"Data Source=sql.cs.luc.edu;Persist Security Info=True;User ID=doszczapinski;Password=p49923";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Signup ss = new Signup();
+            ss.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionStrin))
+            {
+                con.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("Select * from Signup where Username='" + textBox1.Text + "'and Password='" + textBox2.Text + "' ", con);
+                DataTable dt = new System.Data.DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count == 1)
+                {
+                    this.Hide();
+                    Main ss = new Main();
+                    ss.Show();
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Flux/Flux/Signup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Flux
+{
+    public partial class Signup : Form
+    {
+        string connectionString = @"Data Source=sql.cs.luc.edu;Persist Security Info=True;User ID=doszczapinski;Password=p49923";
+
+        public Signup()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+                MessageBox.Show("please fill");
+            else
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand sqlCmd = new SqlCommand("UserADD", con);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@Username", textBox1.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Password", textBox2.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Sign up Worked");
+                    Clear();
+                }
+            }
+        }
+        void Clear()
+        {
+            textBox1.Text = textBox2.Text = "";
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login ss = new Login();
+            ss.Show();
+        }
+    }
+}
