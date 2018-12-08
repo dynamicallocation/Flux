4e42537b9e3dcc9b3f27a765ba583a5324c82028
DanielOsz <doszczapinski@luc.edu>
2018-12-07 21:05:59 -0600

    Working code

--- Flux/Flux/Properties/Settings.Designer.cs
+++ Flux/Flux/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace Flux.Properties {
                 return ((string)(this["EmployeeString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=sql.cs.luc.edu;Initial Catalog=doszczapinski;User ID=doszczapinski;Pa" +
+            "ssword=p49923")]
+        public string doszczapinskiConnectionString {
+            get {
+                return ((string)(this["doszczapinskiConnectionString"]));
+            }
+        }
     }
 }
