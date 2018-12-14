16cff77dfc6fa20f7ce103701cbb03093b397c1c
dynamicallocation <ikavanagh@luc.edu>
2018-12-14 02:09:17 -0600

    added productivity calculator to HW1 code skeleton

---
+++ WindowsFormsApp1/BaseEmployee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRoster
+{
+
+    //employee class for base salary
+    class BaseEmployee : Employee
+    {
+        private decimal baseSalary;
+        private Boolean isProductive = false;
+
+        public BaseEmployee(String firstName, String lastName, String ssn, decimal salary)
+            : base(firstName, lastName, ssn)
+        {
+            baseSalary = salary;
+        }
+
+
+
+        public decimal BaseSalary
+        {
+            get
+            {
+                return baseSalary;
+            } // end get
+            set
+            {
+                baseSalary = (value >= 0) ? value : 0; // validation
+            } // end set
+        } // end property BaseSalary
+
+        // calculate earnings; override method Earnings in CommissionEmployee
+        public override decimal Earnings()
+        {
+            return baseSalary;
+        } // end method Earnings               
+
+        // return string representation of BasePlusCommissionEmployee object
+        public override string ToString()
+        {
+            return "Salaried Employee" + base.ToString() + " " + baseSalary;
+        } // end method ToString 
+        
+
+        public void analyzeBaseProductivity()
+        {
+            if(base.GeneratedRevenue > baseSalary)
+            {
+                isProductive = true;
+            }
+        }
+
+        public Boolean getBaseProductivity()
+        {
+            return isProductive;
+        }
+
+
+
+        
+    } // end class BasePlusCommissionEmployee
+
+
+}
---
+++ WindowsFormsApp1/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRoster
+{
+    abstract class Employee
+    {
+        private String firstName;
+        private String lastName;
+        private String socialSecurityNum;
+        private String userIdentificationNumber;
+        private decimal generatedRevenue;
+        private decimal costPerUnit;
+        private decimal unitsSold;
+        public Employee(String first, String last, String ssn,String uid)
+        {
+
+            firstName = first;
+            lastName = last;
+            socialSecurityNum = ssn;
+            //unique identification number used to input employee sales
+            userIdentifiction number = uid;
+            unitCost = costPerUnit;
+            soldUnits = unitsSold;
+
+        }
+
+        public decimal GeneratedRevenue
+        {
+            get
+            {
+                return generatedRevenue;
+            }
+            set
+            {
+                generatedRevenue = costPerUnit * unitsSold;
+            }
+        }
+
+   
+
+        //This is our employees base class it represents the commanalities all employees share i.e 
+        //every employee will have a salary,ssn,and userid
+    }
+
+}
